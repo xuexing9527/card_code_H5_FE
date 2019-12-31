8c6ccdfd3ef99d8f93ba2268dc8b99b6ac560d09
liuxinyue <996772056@qq.com>
2019-12-31 16:07:59 +0800

    'img'

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="background-color: #fff;height: 100vh">
-        <div style="margin: 2rem 0;display: flex;justify-content: center">
+        <div style="padding: 2rem 0;display: flex;justify-content: center">
             <img src="../assets/images/logo.jpg" style="width: 2.5rem;height: 2.5rem">
         </div>
         <van-row type="flex" justify="center">
